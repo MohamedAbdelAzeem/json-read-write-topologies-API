b615659a9876216a68f3c5b283e947e33e91ae64
Mohamed AbdelAzeem <42822972+MohamedAbdelAzeem@users.noreply.github.com>
2022-01-01 00:31:40 +0200

    test.cpp V1

--- test.cpp
+++ test.cpp
@@ -10,7 +10,7 @@ int main() {
 
     Json::StyledStreamWriter writer; //for writing in json files
 
-
+   //Testing Reader function
     int returnCode = READING_JSON_ERROR;
     if ((returnCode = readJSON("items.json")) != 0)
     {
@@ -18,6 +18,7 @@ int main() {
     }
 
 
+    /* Testing Topology devices Function */
     /*  returnCode = TOPOLOGY_ID_NOT_FOUND;
        vector<string> vect;
        if ((returnCode = TopologyDevices("top1", vect)) != 0)
@@ -26,6 +27,7 @@ int main() {
        }
       */
 
+    /* Testing Device netlist function */
     returnCode = NETLIST_IS_EMPTY;
     vector<string> vect;
     if ((returnCode = DeviceNetlist("top1", "m1", vect)) != 0)
@@ -33,19 +35,20 @@ int main() {
         cout << "Error code :" << returnCode << endl;
     }
 
-
+    /* Printing results */
     for (int i = 0; i < vect.size(); i++)
     {
         cout << vect[i] << endl;
     }
 
-
+   /* Testing Delete Topology  */
    /* returnCode = TOPOLOGY_ID_NOT_FOUND;
     if ((returnCode = DeleteTopology("top1")) != 0)
     {
         cout << "Error code :" << returnCode << endl;
     }*/
 
+    /* Testing Writing Function  */
     returnCode = WRITING_JSON_ID_NOT_FOUND;
     if ((returnCode = writeJson("top1")) != 0)
     {
